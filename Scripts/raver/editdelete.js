3ecf496ca43b2bca00b06533420e4e5a775d603a
unknown <romina@DESKTOP-SH1ON3E.paa.com>
2021-03-01 03:08:21 +0330

    add

---
+++ Scripts/raver/editdelete.js
@@ -0,0 +1,31 @@
+
+	
+
+
+
+
+$(document).ready(function(){
+	
+	
+		 
+   $(function() {
+ $('[data-toggle="popover"]').popover({
+   html: true,
+   content: function() {
+     return $('#popover-content').html();
+   }
+ });
+	 $('body').on('click', function (e) {
+    $('[data-toggle=popover]').each(function () {
+
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+	
+	    });
+
+    });
+
+	
+	})
